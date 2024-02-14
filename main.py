845b98ba076be2726473184c0e4436ff01f75661
Faisal Amin <faisalamin1999@googlemail.com>
2024-02-14 22:32:42 +0800

    commit before changing from conversationchain to non-chain

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ import os
 from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
+from langchain.prompts.prompt import PromptTemplate
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 llm = ChatOpenAI(openai_api_key=openai_api_key, model='gpt-3.5-turbo-0125', temperature=0.5)
+template = """
+{input}
+
+The current conversation history is: {history}
+
+You will play the role of the human and respond to the recipient based on the input provided that is consistent with the conversation history and the reason for the call.
+"""
+prompt_template = PromptTemplate(input_variables=["history", "input"], template=template)
 
 chain = ConversationChain(
     llm = llm,
-    memory = ConversationBufferMemory(),
-    verbose=True
+    memory = ConversationBufferMemory(return_messages=True),
+    verbose=True,
+    prompt=prompt_template
 )
+
+recipient = "Dominos Pizza"
+reason = "ordering pizza"
 
 st.title("ComfortCall")
 
@@ -27,18 +40,26 @@ for message in st.session_state.messages:
         
 if prompt := st.chat_input('What do you want to say next?'):
     
-    with st.chat_message("user"):
-        st.markdown(prompt)
-        
-    st.session_state.messages.append({'role': 'user', 'content': prompt})
+    updated_prompt = f"""
+    Human is holding a phone conversation with: {recipient}
+    The reason for the call is: {reason}
+    Human provides this input: {prompt}
+    """
     
     with st.spinner("Thinking what you should say..."):
-        response = st.session_state.chain.invoke(input = prompt)['response']
+        user_response = st.session_state.chain.invoke(input = updated_prompt)['response']
+        
+    with st.chat_message("user"):
+        st.markdown(user_response)
+        
+    st.session_state.messages.append({'role': 'user', 'content': user_response})
+    
+    recipient_response = 'Yup I got it'
         
     with st.chat_message("Recipient"):
-        st.markdown(response)
+        st.markdown(recipient_response)
         
-    st.session_state.messages.append({'role': 'recipient', 'content': response})
+    st.session_state.messages.append({'role': 'recipient', 'content': recipient_response})
 
 
         
